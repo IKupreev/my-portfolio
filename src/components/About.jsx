6e75943a43e28c6c4434e7bc4c3480277563f75e
unknown <igor.kupreev88@gmail.com>
2022-11-10 16:41:15 +0700

    create new branch about, add html for the about block

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,16 @@
 function About() {
-    return (
-        <section class="about">
-            <div class="about__wrapper">
-                <h1 class="about__title">About</h1>
-            </div>
-        </section>
-    );
+  return (
+    <section class="about">
+      <div class="about__wrapper">
+        <h1 class="about__title">About</h1>
+        <p class="about__description">
+          Web developer, with extensive knowledge and years of experience, working in web
+          technologies and UI/UX design, delivering quality work.
+        </p>
+        <div class="about__experience"></div>
+      </div>
+    </section>
+  );
 }
 
 export default About;

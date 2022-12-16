ac4b632352c6ebca6869698ce822d319fe86dab3
unknown <igor.kupreev88@gmail.com>
2022-12-16 15:54:39 +0700

    added styles for skills block

--- src/components/SkillsBlock/index.jsx
+++ src/components/SkillsBlock/index.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 import Icons from '../Icons';
 
-const ExpirienceBlock = ({nameIcon, title, progress}) => {
+import styles from './ExpirienceBlock.module.scss';
+
+const ExpirienceBlock = ({ nameIcon, title, progress }) => {
   return (
-    <div>
-        <Icons
-          name={nameIcon}
-          width="27.8"
-          height="30"
-          color='#383838'
-          className="icon"
-        />
+    <div className={styles.skillsBlock}>
+      <Icons name={nameIcon} width="27.8" height="30" className={styles.icon} />
+      <div className={styles.info}>
         <h3>{title}</h3>
         <label>{progress}%</label>
+      </div>
+      <Icons name="wave" width="170" height="17" color="#84B1DB" />
+      <div className={styles.line} style={{ background: '#84B1DB' }}></div>
     </div>
-  )
-}
+  );
+};
 export default ExpirienceBlock;
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ExperienceBlock from '../components/SkillsBlock'
+import ExperienceBlock from '../components/SkillsBlock';
 
 const Skills = () => {
   const [frontend, setfrontend] = React.useState([]);
@@ -25,31 +25,33 @@ const Skills = () => {
       });
   }, []);
   return (
-  <section className="skills">
-    <div className="skills__wrapper">
-      <h1 className="skills__title">Skills</h1>
-      <h1 className="skills__subtitle">My technical level</h1>
-        <section className="skills__oneBlock">
-          <h2 className="skills__block-title">Frontend Developer</h2>
-          <label className="skills__label">
-            More than 4 years
-          </label>  
-          <div className='sliils__experience'>
-            <div className='slills__item'>
-              
+    <main className="skills">
+      <div className="skills__wrapper">
+        <h1 className="skills__title">Skills</h1>
+        <h1 className="skills__subtitle">My technical level</h1>
+        <div className="skills__container">
+          <section className="skills__oneBlock">
+            <h2 className="skills__block-title">Frontend Developer</h2>
+            <label className="skills__subtitle">More than 4 years</label>
+            <div className="skills__experience">
+              {frontend.map((obj) => (
+                <ExperienceBlock key={obj.id} {...obj} />
+              ))}
             </div>
-          </div>  
-        </section>
-        <section className="skills__twoBlock">
-          <h1 className="skills__block-title">Designer</h1>
-          <span className="skills__label">More than 2 years</span>
-          <div className='sliils__experience'>
-
-          </div>
-        </section>
-    </div>
-  </section>
-);
-}
+          </section>
+          <section className="skills__twoBlock">
+            <h1 className="skills__block-title">Designer</h1>
+            <span className="skills__subtitle">More than 2 years</span>
+            <div className="skills__experience">
+              {designer.map((obj) => (
+                <ExperienceBlock key={obj.id} {...obj} />
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Skills;

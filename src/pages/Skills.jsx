f63ed223b23d6b887f81811b2b74dafc99a58f8e
unknown <igor.kupreev88@gmail.com>
2023-01-09 13:53:08 +0700

    added skeleton for skills block

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import ExperienceBlock from '../components/SkillsBlock';
+import Skeleton from '../components/Cards/SkeletonCards';
 
 const Skills = () => {
   const [frontend, setfrontend] = React.useState([]);
   const [designer, setDesigner] = React.useState([]);
+  const [isLoading, setisLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://638c8f9feafd555746a8fe43.mockapi.io/frontend')
@@ -13,6 +15,7 @@ const Skills = () => {
       })
       .then((arr) => {
         setfrontend(arr);
+        setisLoading(false);
       });
   }, []);
   React.useEffect(() => {
@@ -34,18 +37,18 @@ const Skills = () => {
             <h2 className="skills__block-title">Frontend Developer</h2>
             <label className="skills__subtitle">More than 4 years</label>
             <div className="skills__experience">
-              {frontend.map((obj) => (
-                <ExperienceBlock key={obj.id} {...obj} />
-              ))}
+              {isLoading
+                ? [...new Array(4)].map((_, index) => <Skeleton key={index} />)
+                : frontend.map((obj) => <ExperienceBlock key={obj.id} {...obj} />)}
             </div>
           </section>
           <section className="skills__twoBlock">
             <h1 className="skills__block-title">Designer</h1>
             <span className="skills__subtitle">More than 2 years</span>
             <div className="skills__experience">
-              {designer.map((obj) => (
-                <ExperienceBlock key={obj.id} {...obj} />
-              ))}
+              {isLoading
+                ? [...new Array(4)].map((_, index) => <Skeleton key={index} />)
+                : designer.map((obj) => <ExperienceBlock key={obj.id} {...obj} />)}
             </div>
           </section>
         </div>
